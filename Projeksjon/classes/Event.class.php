f1a37736535e541b6bea5b04649038a48753a7ad
tb <tb@78.91.3.147>
2012-09-25 14:39:43 +0200

    Updated events

--- Projeksjon/classes/Event.class.php
+++ Projeksjon/classes/Event.class.php
@@ -19,11 +19,12 @@ Hva som trengs:
 */	
 class Event
 {
-	private $id, $date, $place, $description, $signupStart, $signupStop, $yearRestriction, $attendees, $openForEveryOne;
+	private $id, $date, $place, $description, $signupStart, $signupStop, $yearRestriction, $attendees, $openForEveryOne, $maxPersons, $createdBy;
 
-	function __construct($date, $place,$description,$signupStart,
-						$signupStop,$yearRestriction, $openForEveryone)
+	function __construct($id, $date, $place,$description,$signupStart,
+						$signupStop,$yearRestriction, $maxPersons, $createdBy)
 	{
+		$this->id = $id;
 		$this->date = $date;
 		$this->place = $place;
 		$this->description = $description;
@@ -31,16 +32,44 @@ class Event
 		$this->signupStop = $signupStop;
 		$this->yearRestriction = $yearRestriction;
 		$this->openForEveryOne = $openForEveryone;
+		$this->createdBy = $createdBy;
+		$this->maxPersons = $maxPersons;
 		
 	}
 	
-	public static function retrieveEvent($eventid)
+	
+	public function updateEvent($id, $date, $place,$description,$signupStart,
+						$signupStop,$yearRestriction, $maxPersons, $createdBy)
 	{
+		throw new Exception("Not implemented yet!");						
 	}
+	
+	
+	
+	public static function retrieveEvent($eventid, $dbh)
+	{
+		$query = sprintf("SELECT * FROM `event` WHERE `id` = '%s' LIMIT 1;", mysql_real_escape_string($id));
+		
+		foreach ($dbh->query($query) as $row) {
+			$event = $row;
+		}
+		$person = Person::retrievePerson($event['id'], $dbh);
+		return new Event($event['id'], $event['time'],$event['place'],$event['description'],
+				$event['regStart'], $event['regStop'],$event['minGrad'],$event['maxPers'],$person);
+	}
+	
 	public static function retrieveUpcomingEvents($no) {
 		//Retreives the specified number of upcomming events
-		//If $no equals 0 retrieve all upcomming events.
+		//If $no equals 0 retrieve all upcomming events ordered by date.
+		$query = sprintf("SELECT * FROM `event` ORDER BY `event`.`time` ASC LIMIT 0 , %s;", $no);
+		$events = array();
+		foreach ($dbh->query($query) as $event) {
+			$person = Person::retrievePerson($event['id'], $dbh);
+			$events[] = new Event($event['id'], $event['time'],$event['place'],$event['description'],
+				$event['regStart'], $event['regStop'],$event['minGrad'],$event['maxPers'],$person);
+		}
 		
+		return $events;
 		
 	}
 	
